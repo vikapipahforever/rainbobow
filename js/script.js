c0a698d309241f1dd70762f2257e5b3990ee40a7
vikapipahforever <110288326+vikapipahforever@users.noreply.github.com>
2022-07-30 19:54:11 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// audio
+function play() {
+	var audio = document.getElementById("audio");
+	audio.play();
+}
+
+// Animate On Scroll Library : https://michalsnik.github.io/aos/
+AOS.init({
+  delay: 200,
+  duration: 1000
+});
